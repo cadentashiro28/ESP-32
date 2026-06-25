b675e37143413e95f8323ea0ee880b80d0c2f248
Caden Tashiro <cadentashiro@gmail.com>
2026-06-24 21:15:02 -0700

    Added WHO AM I check

--- MPU6050-I2C/main/main.c
+++ MPU6050-I2C/main/main.c
@@ -3,6 +3,27 @@
 #include <soc/i2c_struct.h>
 #include <driver/i2c_master.h>
 
+#define MPU6050_ADDR 0x68
+#define WHO_AM_I_REG 0x75
+#define SMPRT_DIV_REG 0x19
+#define CFG_REG 0x1A
+
+typedef struct {
+    uint8_t smprt_div;
+
+
+} MPU6050_Config;
+
+void i2c_write_reg(i2c_master_dev_handle_t dev_handle, uint8_t reg_address, uint8_t reg_value, uint8_t write_len) {
+    uint8_t write_buf[] = {reg_address, reg_value};
+    ESP_ERROR_CHECK(i2c_master_transmit(dev_handle, write_buf, write_len, -1));
+}
+
+void i2c_read_reg(i2c_master_dev_handle_t dev_handle, uint8_t reg_address, uint8_t *read_buf, uint8_t read_len) {
+    uint8_t write_buf[] = {reg_address};
+    ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, write_buf, 1, read_buf, read_len, -1));
+}
+
 void i2c_init(i2c_master_dev_handle_t *dev_handle) {
     i2c_master_bus_config_t i2c_mst_config = {
         .clk_source = I2C_CLK_SRC_DEFAULT,
@@ -23,19 +44,22 @@ void i2c_init(i2c_master_dev_handle_t *dev_handle) {
     };
 
     ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, dev_handle));
+    uint8_t read_buf;
+    i2c_read_reg(*dev_handle, WHO_AM_I_REG, &read_buf, 1);
+    if (MPU6050_ADDR == read_buf) {
+        printf("I2C Successfully connected to MPU6050 at address 0x%X\n", (unsigned int) read_buf);
+    } else {
+        printf("I2C failed to establish a connection, received address %X, expected %X\n", (unsigned int) read_buf, MPU6050_ADDR);
+    }
 }
 
-void i2c_write_reg(i2c_master_dev_handle_t dev_handle, uint8_t reg_address, uint8_t reg_value, uint8_t write_len) {
-    uint8_t write_buf[] = {reg_address, reg_value};
-    ESP_ERROR_CHECK(i2c_master_transmit(dev_handle, write_buf, write_len, -1));
+void mpu6050_init_cfg(i2c_master_dev_handle_t *dev_handle) {
+    i2c_write_reg(*dev_handle, SMPRT_DIV_REG, 0b0, 1); // smprt = gyro_rt / (1 + div)
 }
 
-void i2c_read_reg(i2c_master_dev_handle_t dev_handle, uint8_t reg_address, uint8_t *read_buf, uint8_t read_len) {
-    uint8_t write_buf[] = {reg_address};
-    ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, write_buf, 1, read_buf, read_len, -1));
-}
 
 void app_main() {
     i2c_master_dev_handle_t dev_handle;
     i2c_init(&dev_handle);
+
 }
